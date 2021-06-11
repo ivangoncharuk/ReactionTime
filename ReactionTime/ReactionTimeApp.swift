ada96650dd6d5e789496b3ab3bc836e1c67e3189
Ivan Goncharuk <igonch1@students.towson.edu>
2021-06-11 07:42:15 -0400

    Initial Commit

---
+++ ReactionTime/ReactionTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionTimeApp.swift
+//  ReactionTime
+//
+//  Created by Ivan Goncharuk on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ReactionTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
