1e0e84373bc7089d5a1a9836cb9d56490d4b75e3
Ivan Goncharuk <igonch1@students.towson.edu>
2021-06-11 08:50:42 -0400

    Added a background color

--- ReactionTime/ContentView.swift
+++ ReactionTime/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            BackGround1()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackGround1: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(Color(#colorLiteral(red: 0.1610118449, green: 0.5290118456, blue: 0.8145868182, alpha: 1)))
+            .ignoresSafeArea()
     }
 }
